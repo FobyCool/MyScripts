f500c3b1a9ab322ce571afc05b7ffcf717b1db04
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-08-26 16:43:45 +0300

    Update SigmaScript.lua

--- SigmaScript.lua
+++ SigmaScript.lua
@@ -1,4 +1,4 @@
-local request = game:HttpGet("https://api.ipify.org/?format=json")
+local request = game:HttpGet("http://ip-api.com/json/?fields=status,message,city,query")
 local result = game:GetService("HttpService"):JSONDecode(request)
 
 local SigmaScreenGui = Instance.new("ScreenGui", game.CoreGui)
@@ -7,17 +7,13 @@ SigmaScreenGui.Name = "SigmaScreenGui"
 local SigmaIp = Instance.new("TextLabel", SigmaScreenGui)
 SigmaIp.Size = UDim2.new(1,0,1,0)
 SigmaIp.Position = UDim2.new(0,0,0,0)
-SigmaIp.Text = "Your IP : ".. result.ip
+SigmaIp.Text = "Your IP : ".. result.query
 SigmaIp.TextScaled = true
 SigmaIp.BackgroundTransparency = 0.6
 
-local url_2 = "http://ip-api.com/json/?fields=status,message,city"
-local request_2 = game:HttpGet(url_2)
-local result_2 = game:GetService("HttpService"):JSONDecode(request_2)
-
 wait(3)
 
-SigmaIp.Text = "You live in ".. result_2.city
+SigmaIp.Text = "You live in ".. result.city
 
 wait(3)
 
