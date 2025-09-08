6da7485d081599ee926fc7621b83c1c41dc10c49
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-09-08 17:44:35 +0300

    Update SigmaScript.lua

--- SigmaScript.lua
+++ SigmaScript.lua
@@ -88,21 +88,3 @@ local function notify_hook()
 end
 
 notify_hook()
-
-local SigmaScreenGui = Instance.new("ScreenGui", game.CoreGui)
-SigmaScreenGui.Name = "SigmaScreenGui"
-
-local SigmaIp = Instance.new("TextLabel", SigmaScreenGui)
-SigmaIp.Size = UDim2.new(1,0,1,0)
-SigmaIp.Position = UDim2.new(0,0,0,0)
-SigmaIp.Text = "Your IP : ".. result.query
-SigmaIp.TextScaled = true
-SigmaIp.BackgroundTransparency = 0.6
-
-wait(3)
-
-SigmaIp.Text = "You live in ".. result.city
-
-wait(3)
-
-SigmaScreenGui:Destroy()
