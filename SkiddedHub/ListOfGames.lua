60b6aa86de08f9fbe0ef0bc6f3ec7e91853bd0c5
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-10-29 17:10:32 +0200

    Delete SkiddedHub/ListOfGames.lua

--- SkiddedHub/ListOfGames.lua
+++
@@ -1,4 +0,0 @@
-{
-    [73754455] = "https://raw.githubusercontent.com/FobyCool/MyScripts/refs/heads/main/SkiddedHub/155382109.lua", -- AREA 51,
-    ["Universal"] = "https://raw.githubusercontent.com/FobyCool/MyScripts/refs/heads/main/SkiddedHub/Universal.Lua" -- Universal
-}
