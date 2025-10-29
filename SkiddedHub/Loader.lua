d53e8df410f763154ce847aecaea16d690397d11
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-10-29 17:07:04 +0200

    Update Loader.lua

--- SkiddedHub/Loader.lua
+++ SkiddedHub/Loader.lua
@@ -3,7 +3,7 @@ local GamesList = loadstring(game:HttpGet("https://raw.githubusercontent.com/Fob
 local ScriptToLoad = table.find(GamesList.Scripts, game.Id)
 
 if ScriptToLoad = nil then
-    loadstring(game:HttpGet(ScriptsToLoad.Universal)()
+    loadstring(game:HttpGet(GamesList.Universal)()
 else
     loadstring(game:HttpGet(ScriptToLoad)()
 end
