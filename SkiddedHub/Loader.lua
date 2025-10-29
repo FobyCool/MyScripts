6a93e1f571a7c46d656e97a6d47bd9f9c3bf9753
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-10-29 17:18:00 +0200

    Update Loader.lua

--- SkiddedHub/Loader.lua
+++ SkiddedHub/Loader.lua
@@ -1,15 +1,10 @@
-local GamesList = 
-
-{
+local GamesList = {
     [73754455] = "https://raw.githubusercontent.com/FobyCool/MyScripts/refs/heads/main/SkiddedHub/155382109.lua", -- AREA 51,
     ["Universal"] = "https://raw.githubusercontent.com/FobyCool/MyScripts/refs/heads/main/SkiddedHub/Universal.Lua" -- Universal
 }
 
-
-local ScriptToLoad = table.find(GamesList.Scripts, game.GameId)
-
-if ScriptToLoad == nil then
-    loadstring(game:HttpGet(GamesList.Universal))()
+if GamesList then
+    loadstring(game:HttpGet(GamesList[game.GameId]))()
 else
-    loadstring(game:HttpGet(ScriptToLoad))()
+    loadstring(game:HttpGet(GamesList["Universal"]))()
 end
