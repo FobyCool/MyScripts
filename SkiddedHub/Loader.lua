e75df089dbd4ec1102e96d438b779757f72a676c
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-10-29 16:47:05 +0200

    Create Loader.lua

---
+++ SkiddedHub/Loader.lua
@@ -0,0 +1,9 @@
+local GamesList = loadstring(game:HttpGet("sigmaurl"))()
+
+local ScriptToLoad = table.find(GamesList.Scripts, game.Id)
+
+if ScriptToLoad = nil then
+    loadstring(game:HttpGet("Universal")()
+else
+    loadstring(game:HttpGet(ScriptToLoad)()
+end
