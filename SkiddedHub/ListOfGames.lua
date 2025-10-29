f22b85efe16b81144774fa2b05e630459cd71bfa
FobyCool <168823103+FobyCool@users.noreply.github.com>
2025-10-29 16:50:06 +0200

    Create ListOfGames.lua

---
+++ SkiddedHub/ListOfGames.lua
@@ -0,0 +1,4 @@
+local Scripts = {
+    155382109 = "https://raw.githubusercontent.com/FobyCool/MyScripts/refs/heads/main/SkiddedHub/155382109.lua" -- AREA 51,
+    Universal = "print('Work In Progress')" --
+}
